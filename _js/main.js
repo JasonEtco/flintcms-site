97fee5eab8eacc121006b054658f1b44b9697002
JasonEtco <jasonetco@gmail.com>
2017-06-03 14:12:24 -0400

    :man: Design header/nav

--- _js/main.js
+++ _js/main.js
@@ -1,3 +1,12 @@
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    header.classList.add('has-scrolled');
+  } else {
+    header.classList.remove('has-scrolled');
+  }
+})
+
 const homeBtns = Array.from(document.querySelectorAll('.js-home-btn'));
 if (homeBtns) {
   const homeBlocks = Array.from(document.querySelectorAll('.js-home-block'));
