3bf0a6d40d4b792935202981abc503407773afba
JasonEtco <jasonetco@gmail.com>
2017-06-09 22:58:08 -0400

    :phone: Responsive nav

--- _js/main.js
+++ _js/main.js
@@ -1,11 +1,24 @@
 const header = document.querySelector('.header');
+const headerOverlay = document.querySelector('.header__overlay');
+const navButton = document.querySelector('.nav__button');
+
 window.addEventListener('scroll', () => {
   if (window.scrollY > 400) {
     header.classList.add('has-scrolled');
   } else {
     header.classList.remove('has-scrolled');
   }
-})
+});
+
+if (navButton && header && headerOverlay) {
+  navButton.addEventListener('click', () => {
+    header.classList.toggle('is-active');
+  });
+
+  headerOverlay.addEventListener('click', () => {
+    header.classList.remove('is-active');
+  });
+}
 
 const homeBtns = Array.from(document.querySelectorAll('.js-home-btn'));
 if (homeBtns) {
